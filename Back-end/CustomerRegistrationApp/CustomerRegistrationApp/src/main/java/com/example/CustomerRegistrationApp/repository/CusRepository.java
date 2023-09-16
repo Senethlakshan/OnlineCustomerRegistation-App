46413f811c2236994814d628796847bfa1154706
Senethlakshan <81217773+Senethlakshan@users.noreply.github.com>
2023-09-16 12:08:45 +0530

    crud functions woking

--- Back-end/CustomerRegistrationApp/CustomerRegistrationApp/src/main/java/com/example/CustomerRegistrationApp/repository/CusRepository.java
+++ Back-end/CustomerRegistrationApp/CustomerRegistrationApp/src/main/java/com/example/CustomerRegistrationApp/repository/CusRepository.java
@@ -4,4 +4,5 @@ import com.example.CustomerRegistrationApp.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CusRepository extends JpaRepository<Customer, Long> {
+
 }
